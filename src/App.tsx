5807af2e5a00009cb6b5a8339063fc4db6eae2eb
rodisleyd <rodisleijose@gmail.com>
2026-01-23 21:35:11 -0300

    Add catch-all route to fix white screens

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const App: React.FC = () => {
                         </ProtectedRoute>
                     }
                 />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
     );
